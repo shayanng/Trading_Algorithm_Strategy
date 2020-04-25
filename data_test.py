8d2d74a0510315d8f3faf6eaa9aabe98b9bf74e6
rezan21 <reza.naghshineh@gmail.com>
2020-04-25 20:50:45 +0100

    multiple ATR v2

--- data_test.py
+++ data_test.py
@@ -2,11 +2,21 @@ from utils import data_call as dc
 from utils import tech_indicators as ti
 import pandas as pd
 
-ASSET = "IBM"
-df = pd.read_csv(f"./data/data_{ASSET}.csv") # read data
+
+TICKERS = ["IBM", "AAPL", "TSLA", "NVDA"]
 atr_periods = [3, 7, 55]
 
-for i in atr_periods:
-    df[f"ATR_{i}"] = ti.ATR(df, i)
+# Yahoo API
+generated_data_daily = dc.generate_data(TICKERS, interval="d", n_per=90)
 
-print(df)
+data_with_atr = {}
+for k,v in generated_data_daily.items():
+    v_ = v.copy()
+    for i in atr_periods:
+        v_[f"ATR_{i}"] = ti.ATR(v_, i)
+    data_with_atr.update({k:v_})
+    
+print(data_with_atr)
+
+# print(data_with_atr.head())
+# data_with_atr.to_csv("ssss.csv")
